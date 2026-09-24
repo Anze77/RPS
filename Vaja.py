7b23116260717c9c36aa263cccfa92a931d15dbd
Anze77 <anze.hafo@gmail.com>
2026-09-24 11:40:37 +0200

    Add files via upload

---
+++ Vaja.py
@@ -0,0 +1,35 @@
+def vnos(s:list):
+    while True:
+        izdelek = input ("Vnesi: ").lower()
+        if izdelek == "":
+            break
+        s.append(izdelek)
+
+def izpis(s:list):
+    print("-----------------")
+    for index in range(len(s)):
+        print(f"({index+1}) {s[index]}")
+    
+def briši(s:list):
+    brisi = input("Briši: ").lower()
+    print("Brišem: ", end="")
+    while brisi in s:
+        s.remove(brisi)
+        print("*", end="")
+    print()
+    
+def posodobi(s:list):
+    stara = input("Posodobi: ")
+    nova= input("Novo: ")
+    for index in range(len(s)):
+        if stara == s[index]:
+            s[index] = nova
+    
+if __name__ == "__main__":
+    db = []
+    vnos(db)
+    izpis(db)
+    briši(db)
+    izpis(db)
+    posodobi(db)
+    izpis(db)
